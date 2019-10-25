2af488842039b77522d7caea1c523adb2af70d4c
zshuaigit <zshuai1722@163.com>
2019-10-25 10:24:14 +0800

    在1.0上修复了用户反馈的bug

--- src/com/zshuai/demo/controller/UserController.java
+++ src/com/zshuai/demo/controller/UserController.java
@@ -2,6 +2,10 @@ package com.zshuai.demo.controller;
 
 public class UserController {
     public void getUserInfo(){
+
         System.out.println("获取用户信息方法类！");
+
+        //修改用户反馈的bug
+        System.out.println("修改好了用户反馈的bug");
     }
 }
